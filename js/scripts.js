02a3c75b021ce14721961bdbfd09d2a8bf9b7c8d
Bruno Zablotny <rusholdfield@gmail.com>
2021-03-17 14:51:24 +0100

    Added an array with Pokemon Objects

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,43 @@
-alert('Hello world');
-let favouriteFood = "Sushi";
-document.write(favouriteFood);
+let pokemonList = [];
+pokemonList = [
+    {
+        name: "Bulbasaur",
+        height: "2'04\"",
+        weight: "15.2 lbs",
+        category: "Seed",
+        abilities: "Overgrow",
+        type: ["Grass", "Poison"]
+    },
+    {
+        name: "Ivysaur",
+        height: "3'03\"",
+        weight: "28.7 lbs",
+        category: "Seed",
+        abilities: "Overgrow",
+        type: ["Grass", "Poison"]
+    },
+    {
+        name: "Venusaur",
+        height: "6'07\"",
+        weight: "220.5 lbs",
+        category: "Seed",
+        abilities: "Overgrow",
+        type: ["Grass", "Poison"]
+    },
+    {
+        name: "Bulbasaur",
+        height: "2'04\"",
+        weight: "15.2 lbs",
+        category: "Seed",
+        abilities: "Overgrow",
+        type: ["Grass", "Poison"]
+    },
+    {
+        name: "Charmander",
+        height: "2'00\"",
+        weight: "18.7 lbs",
+        category: "Lizard",
+        abilities: "Blaze",
+        type: "Fire"
+    }
+]
